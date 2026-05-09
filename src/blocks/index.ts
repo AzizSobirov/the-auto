b8aa054ee13f03ab3591e526f6f6fabe1c552a97
Aziz <azizsobirovhazarasp@gmail.com>
2026-05-09 19:35:21 +0500

    feat: add About block, H5 component, and remove redundant font preloads

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,2 +1,3 @@
+export { default as About } from "./About.astro";
 export { default as Advantages } from "./Advantages.astro";
 export { default as Hero } from "./Hero.astro";
--- src/components/ui/Text/index.ts
+++ src/components/ui/Text/index.ts
@@ -2,4 +2,5 @@ export { default as H1 } from "./H1.astro";
 export { default as H2 } from "./H2.astro";
 export { default as H3 } from "./H3.astro";
 export { default as H4 } from "./H4.astro";
+export { default as H5 } from "./H5.astro";
 export { default as P } from "./P.astro";
