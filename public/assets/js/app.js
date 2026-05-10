adaa531155b2dca5f8c1faa7e76debe1f52dc823
Aziz <azizsobirovhazarasp@gmail.com>
2026-05-11 03:14:55 +0500

    feat: reduce scroll threshold for header transition to 20px

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -177,7 +177,7 @@ if (header) {
   })
 
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 20) {
       header.classList.add('header--scrolled')
     } else {
       header.classList.remove('header--scrolled')
